3b6fb945852bcc732562d3a23426c71ae4d2a278
mimibetty <you@example.com>
2024-04-08 21:40:34 +0700

    init

---
+++ a.py
@@ -0,0 +1,18 @@
+
+print(300)
+
+#nooooo Thanh Minh
+
+print("MU vo doi")
+#thoi xoa het toi lam lai 
+print("ahihi do ngoc")
+
+
+s = "Thhanh Minh"
+print(s)
+
+
+
+print(1 + 1)
+def Xoa(a):
+    return a+1;
